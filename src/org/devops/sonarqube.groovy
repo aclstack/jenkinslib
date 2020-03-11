8d5508656910b6c1cc0d0c649e71b2da88a7f93a
aclstack <aclstack@gmail.com>
2020-03-11 10:42:28 +0800

    PHP版本扫描

--- src/org/devops/sonarqube.groovy
+++ src/org/devops/sonarqube.groovy
@@ -1,3 +1,21 @@
 package org.devops
 
-def SonarScan(projectName,)
+def SonarScan(projectName,projectDesc,projectPath){
+    withSonarQubeEnv(credentialsId: 'SonaerServer-Admin') {
+      def scannerHome = "/usr/local/sonar-scanner"
+      def sonarDate = sh returnStdout: true, script: 'date +%Y%m%d%H%M%S'
+      sonarDate = sonarDate - "\n"
+      
+      
+      sh """
+          ${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=${projectName} \
+          -Dsonar.projectName=${projectName} \
+          -Dsonar.projectVersion=${sonarDate} \
+          -Dsonar.ws.timeout=30 \
+          -Dsonar.projectDescription=${projectDesc} \          
+          -Dsonar.language=php \
+          -Dsonar.dynamicAnalysis=false  \
+          -Dsonar.sourceEncoding=UTF-8
+      """
+  }
+}
